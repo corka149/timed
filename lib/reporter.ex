046e54361392f3ecea1b1a76ffef1030f3beca63
corka149 <corka149@mailbox.org>
2019-05-08 20:14:40 +0200

    Refactored report to make it more "elixir"-like

--- lib/reporter.ex
+++ lib/reporter.ex
@@ -2,41 +2,39 @@ defmodule Timed.Reporter do
 
   require Logger
 
-  def log_statistics(timed_list) do
-    timed_list
-    |> log_hours_today()
-    |> log_overtime()
-  end
-
   @doc """
-  Logs to stdout the worked hours for today.
+  Checks a list of timed structures and search for the entry which of the current day.
+  For this entry it will calculate and return the worked hours.
   """
-  def log_hours_today(timed_list) do
-    log_hours(timed_list)
-    timed_list
-  end
+  @spec get_hours_today(maybe_improper_list()) :: nil | float()
+  def get_hours_today([]) do nil end
 
-  defp log_hours([]) do nil end
-
-  defp log_hours([head | tail]) do
+  def get_hours_today([head | tail]) do
     %{start: start} = head
     if NaiveDateTime.to_date(start) == Date.utc_today() do
-      worked_hours =  calc_worked_hours(head)
-      Logger.info("Hours today: #{worked_hours} hrs")
+      calc_worked_hours(head)
     else
-      log_hours_today(tail)
+      get_hours_today(tail)
     end
   end
 
   @doc """
-  Logs to stdout the difference between expected sum of hours and sum of actual hours
+  Calculates the difference between expected sum of hours and sum of actual hours
+
+
+  ## Examples
+
+      iex> t1 = %Timed{start: ~N/2019-02-11 08:00:00/, end: ~N/2019-02-11 18:00:00/, break: 60} # 1hr
+      iex> t2 = %Timed{start: ~N/2019-02-12 07:30:00/, end: ~N/2019-02-12 17:30:00/, break: 30} # 1.5hr
+      iex> timed_list = [t1, t2]
+      iex> Timed.Reporter.calc_total_overtime timed_list
+      2.5
   """
-  def log_overtime(timed_list) do
+  @spec calc_total_overtime([Timed.t()]) :: number()
+  def calc_total_overtime(timed_list) do
     expected_worked_hours = 8 * length(timed_list)
     actual_worked_hours = Enum.reduce(timed_list, 0, fn timed, acc -> calc_worked_hours(timed) + acc end)
-    overtime = actual_worked_hours - expected_worked_hours
-    Logger.info("Overtime: #{overtime} hrs")
-    timed_list
+    actual_worked_hours - expected_worked_hours
   end
 
   @doc """
@@ -45,14 +43,39 @@ defmodule Timed.Reporter do
 
   ## Examples
 
-      iex> timed = %Timed{%Timed{} | start: ~N[2019-04-01 08:00:00]}
-      iex> timed = %Timed{timed | end: ~N[2019-04-01 17:00:00]}
-      iex> timed = %Timed{timed | break: 30}
+      iex> timed = %Timed{start: ~N[2019-04-01 08:00:00], end: ~N[2019-04-01 17:00:00], break: 30}
       iex> Timed.Reporter.calc_worked_hours timed
       8.5
   """
+  @spec calc_worked_hours(Timed.t()) :: float()
   def calc_worked_hours(%Timed{start: start_datetime, end: end_datetime, break: break}) do
     diff_seconds = NaiveDateTime.diff end_datetime, start_datetime
     (diff_seconds / 60 / 60) - (break / 60)
+  end
+
+  @doc """
+  Works like IO.inspect. It takes a list of time structs, calculates total overtime and worked hours today
+  and return the provided, untouched list.
+  """
+  def log_statistics(timed_list) do
+    timed_list
+    |> log_hours_today()
+    |> log_total_overtime()
+  end
+
+  # ===== Convenience wrapper for logging =====
+
+  # Wrapper for piping
+  def log_total_overtime(timed_list) do
+    overtime = calc_total_overtime(timed_list)
+    Logger.info("Overtime: #{overtime} hrs")
+    timed_list
+  end
+
+  # Wrapper for piping
+  defp log_hours_today(timed_list) do
+    worked_hours = get_hours_today(timed_list)
+    Logger.info("Hours today: #{worked_hours} hrs")
+    timed_list
   end
 end
