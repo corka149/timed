e020ef224a1b4622c7969a513a9df4286df239d6
corka149 <corka149@mailbox.org>
2020-05-18 21:02:27 +0200

    Added amqp primitives

--- timed_server/apps/communicator/lib/communicator.ex
+++ timed_server/apps/communicator/lib/communicator.ex
@@ -1,18 +1,27 @@
 defmodule Communicator do
   @moduledoc """
-  Documentation for `Communicator`.
+  The communicator encapsulate the communication between the clients and persistence layer.
   """
 
-  @doc """
-  Hello world.
+  def send_and_receive do
+    {:ok, conn} = AMQP.Connection.open(username: "admin", password: "s3cr3t", virtual_host: "timed_vhost")
+    {:ok, chan} = AMQP.Channel.open(conn)
 
-  ## Examples
+    AMQP.Queue.declare(chan, queue())
+    AMQP.Exchange.declare(chan, exchange())
+    AMQP.Queue.bind(chan, queue(), exchange())
 
-      iex> Communicator.hello()
-      :world
+    AMQP.Basic.publish(chan, exchange(), "", "First message =)")
 
-  """
-  def hello do
-    :world
+    {:ok, payload, _meta} = AMQP.Basic.get(chan, queue())
+    IO.puts payload
+  end
+
+  def exchange do
+    "working_hours_exchange"
+  end
+
+  def queue do
+    "working_hours_queue"
   end
 end
