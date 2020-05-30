d04bb76f1bd43573f674b132baabacb90ffb7643
corka149 <corka149@mailbox.org>
2020-05-30 15:43:46 +0200

    Updated usage to new version

--- README.md
+++ README.md
@@ -8,11 +8,19 @@ Manages my working times. This project is mainly for tinkering around with Elixi
 ```
 -d, --date          Takes the date that should be used. Format: "yyyy-mm-dd" -> E.g.
                     2019-03-28. When not provided, it will use the current date.
--t, --time          Can take start and/or end. Format "hh:mm" -> E.g. "08:00~17:00",
-                    "~16:45", "07:30~". When no entry exists, it will use the current
-                    time for the missing time.
+
+-s, --start         Takes the start time. Format "hh:mm" -> E.g. "08:00".
+                    When the parameter is not provided it will use the current time 
+                    for the missing time.
+
+-e, --end           Parameter for end time. Format "hh:mm" -> E.g. "08:00".
+                    When the parameter is not provided it will use the current time 
+                    for the missing time.
+
 -b, --break         Takes the duration of the break in minutes. Default: 0min
+
 -n, --note          Takes a note and add it to an entry.
+
 ```
 
 ## Data
--- client/lib/cli.ex
+++ client/lib/cli.ex
@@ -40,17 +40,21 @@ defmodule Timed.Cli do
       Manages your working times.
 
     Usage:
-      -d, --date          Takes the date that should be used. Format: "yyyy-mm-dd" -> E.g.
-                          2019-03-28. When not provided, it will use the current date.
-      -t, --time          Can take start and/or end. Format "hh:mm" -> E.g. "08:00~17:00",
-                          "~16:45", "07:30~". When no entry exists, it will use the current
-                          time for the missing time.
-      -b, --break         Takes the duration of the break in minutes. Default: 0min
-      -n, --note          Takes a note and add it to an entry.
+    -d, --date          Takes the date that should be used. Format: "yyyy-mm-dd" -> E.g.
+                        2019-03-28. When not provided, it will use the current date.
 
-    Data:
-      Timed data is stored in "$HOME/.timed.csv". The columns are structured the following way:
-      date, start, end, breaktime, note
+    -s, --start         Takes the start time. Format "hh:mm" -> E.g. "08:00".
+                        When the parameter is not provided it will use the current time
+                        for the missing time.
+
+    -e, --end           Parameter for end time. Format "hh:mm" -> E.g. "08:00".
+                        When the parameter is not provided it will use the current time
+                        for the missing time.
+
+    -b, --break         Takes the duration of the break in minutes. Default: 0min
+
+    -n, --note          Takes a note and add it to an entry.
+
     """
   end
 
