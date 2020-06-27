63cb2b5fbf890ca7c6f33e8d63dd22391450796b
corka149 <corka149@mailbox.org>
2020-06-27 22:22:22 +0200

    Bumped version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='timed',
-    version='1.0.0',
+    version='2.0.0',
     packages=[''],
     package_dir={'': 'src'},
     url='https://github.com/corka149/timed/tree/master',
