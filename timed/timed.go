f68fff7cc5638bec014e9b0b1237798add2eecf6
corka149 <corka149@mailbox.org>
2020-10-15 22:20:49 +0200

    Added setter and getter

--- timed/timed.go
+++ timed/timed.go
@@ -36,3 +36,48 @@ func New(id int, day string, brk int, start string, end string, note string) Wor
 		brk, note,
 	}
 }
+
+// ID in the database
+func (wd *WorkingDay) ID() int {
+	return wd.id
+}
+
+// SetStart for change start
+func (wd *WorkingDay) SetStart(start time.Time) {
+	wd.start = start
+}
+
+// Start returns start date & time of work
+func (wd *WorkingDay) Start() time.Time {
+	return wd.start
+}
+
+// SetEnd for changing end
+func (wd *WorkingDay) SetEnd(end time.Time) {
+	wd.end = end
+}
+
+// End returns end date & time of work
+func (wd *WorkingDay) End() time.Time {
+	return wd.end
+}
+
+// SetBrk for changing break
+func (wd *WorkingDay) SetBrk(brk int) {
+	wd.brk = brk
+}
+
+// Brk returns taking break
+func (wd *WorkingDay) Brk() int {
+	return wd.brk
+}
+
+// SetNote for changing note
+func (wd *WorkingDay) SetNote(note string) {
+	wd.note = note
+}
+
+// Note returns description
+func (wd *WorkingDay) Note() string {
+	return wd.note
+}
