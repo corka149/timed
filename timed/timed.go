7ef3805794bc2094f7c8b447860b214089f07d1c
corka149 <corka149@mailbox.org>
2020-10-16 18:52:42 +0200

    Removed verbose getter and setter

--- timed/timed.go
+++ timed/timed.go
@@ -8,13 +8,15 @@ import (
 
 // WorkingDay represents one day of work
 type WorkingDay struct {
-	id int
+	ID int
 
-	start time.Time
-	end   time.Time
+	Day time.Time
 
-	brk  int
-	note string
+	Start time.Time
+	End   time.Time
+
+	Brk  int
+	Note string
 }
 
 func (wd *WorkingDay) String() string {
@@ -23,61 +25,21 @@ func (wd *WorkingDay) String() string {
 
 // New creates a new WorkingDay from query
 func New(id int, day string, brk int, start string, end string, note string) WorkingDay {
-	s, err := time.Parse("2006-01-02 03:04:05.000000", day+" "+start)
+	d, err := time.Parse("2006-01-02", day)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	e, err := time.Parse("2006-01-02 03:04:05.000000", day+" "+end)
+	s, err := time.Parse("03:04:05.000000", start)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e, err := time.Parse("03:04:05.000000", end)
 
 	return WorkingDay{
-		id,
+		id, d,
 		s, e,
 		brk, note,
 	}
-}
-
-// ID in the database
-func (wd *WorkingDay) ID() int {
-	return wd.id
-}
-
-// SetStart for change start
-func (wd *WorkingDay) SetStart(start time.Time) {
-	wd.start = start
-}
-
-// Start returns start date & time of work
-func (wd *WorkingDay) Start() time.Time {
-	return wd.start
-}
-
-// SetEnd for changing end
-func (wd *WorkingDay) SetEnd(end time.Time) {
-	wd.end = end
-}
-
-// End returns end date & time of work
-func (wd *WorkingDay) End() time.Time {
-	return wd.end
-}
-
-// SetBrk for changing break
-func (wd *WorkingDay) SetBrk(brk int) {
-	wd.brk = brk
-}
-
-// Brk returns taking break
-func (wd *WorkingDay) Brk() int {
-	return wd.brk
-}
-
-// SetNote for changing note
-func (wd *WorkingDay) SetNote(note string) {
-	wd.note = note
-}
-
-// Note returns description
-func (wd *WorkingDay) Note() string {
-	return wd.note
 }
