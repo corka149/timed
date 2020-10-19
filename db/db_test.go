355150befa713fe172fb86686666e43fff60a0e7
corka149 <corka149@mailbox.org>
2020-10-19 18:00:51 +0200

    Added update test

--- db/db_test.go
+++ db/db_test.go
@@ -1,4 +1,4 @@
-package tests_test
+package db_test
 
 import (
 	"database/sql"
@@ -31,10 +31,47 @@ func TestInsertAndLoad(t *testing.T) {
 		t.Error("Could not load working day from DB")
 	}
 
+	if wd.Day != wdFromDb.Day && wd.Start == wdFromDb.Start && wd.Brk == wdFromDb.Brk && wd.Note == wdFromDb.Note {
+		t.Error("Working days do not match")
+	}
+
 	past := time.Date(2019, 4, 8, 8, 50, 00, 000, time.Now().Location())
 	wdFromDb = repo.LoadDay(&past)
 	if wdFromDb != nil {
 		t.Error("Loaded a non matching entry")
+	}
+}
+
+func TestUpdateAndLoad(t *testing.T) {
+	dbName := "test_db.db"
+
+	createDb(t, dbName)
+	defer os.Remove(dbName)
+
+	repo := db.NewRepo(dbName)
+	defer repo.Close()
+
+	start := time.Date(2018, 10, 8, 7, 50, 00, 000, time.Now().Location())
+	end := time.Date(2018, 10, 8, 16, 20, 00, 000, time.Now().Location())
+	wd := timed.WorkingDay{Day: start, Start: start, End: end, Brk: 30, Note: "With space"}
+
+	repo.InsertDay(wd)
+
+	wd.Brk = 45
+	wd.Note = "NotSpace"
+	wd.Start = time.Date(2018, 10, 8, 7, 20, 00, 000, time.Now().Location())
+	wd.End = time.Date(2018, 10, 8, 17, 00, 00, 000, time.Now().Location())
+
+	repo.UpdateDay(wd)
+
+	wdFromDb := repo.LoadDay(&start)
+
+	if wdFromDb == nil {
+		t.Error("Could not load working day from DB")
+	}
+
+	if wd.Day != wdFromDb.Day && wd.Start == wdFromDb.Start && wd.Brk == wdFromDb.Brk && wd.Note == wdFromDb.Note && wd.Brk == 45 && wd.Note == "NotSpace" {
+		t.Error("Working days do not match")
 	}
 }
 
